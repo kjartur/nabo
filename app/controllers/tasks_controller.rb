2a0b6714cde709476a1d7c0d9facc4917374a042
Kjartan Manvelyan <kjartan.manvelyan@gmail.com>
2019-05-28 15:31:53 +0200

    Delete task_controller and add tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,23 @@
 class TasksController < ApplicationController
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(task_params)
+  end
+
+  def edit
+    @task = Task.find(task_params)
+  end
+
+  def create
+    Task.create(task_params)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:address, :date, :description, :amount_coins)
+  end
 end
