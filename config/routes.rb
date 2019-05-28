da52bad477274b2549dd6e707475a6a12c65ddbf
William Acevedo <william.acevedo@gmail.com>
2019-05-28 17:15:18 +0200

    adjusted routes to resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   get 'task/new'
   devise_for :users
   root to: 'pages#home'
-<<<<<<< HEAD
+
   resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
   resources :offers do
     resources :reviews, only: [ :new, :create, :edit, :update, :destroy ]
   end
->>>>>>> master
 end
