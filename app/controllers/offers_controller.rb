eebeaabc5f22d9c1e49337872c921441f6be5c56
Julian Hermann <julian.hermann89@gmail.com>
2019-05-28 17:42:58 +0200

    offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,32 @@
 class OffersController < ApplicationController
+  def index
+    @tasks = Task.where(user: current_user)
+    @offers = current_user.offers
+  end
+
+  def show
+  end
+
+  def new
+    @user = current_user
+    @task = Task.find(params[:task_id])
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.user = current_user
+    @offer.task = @task
+    if @offer.save
+      redirect_to confirmed_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:state)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'task/new'
   devise_for :users
   root to: 'pages#home'
-  resources :offers do
+  resources :tasks, only: [ :new, :index ] do
+    resources :offers, only: [ :new, :create ]
+  end
+  resources :offers, only: [ :index, :show ] do
     resources :reviews, only: [ :new, :create, :edit, :update, :destroy ]
   end
 end
