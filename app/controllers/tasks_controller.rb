459e5efeb157cf30574597a18d1b9099107907e2
William Acevedo <william.acevedo@gmail.com>
2019-05-28 16:15:52 +0200

    resoved conflicts

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
   def index
-<<<<<<< HEAD
   end
 
   def show
@@ -13,31 +12,5 @@ class TasksController < ApplicationController
   end
 
   def delete
-=======
-    @tasks = Task.all
-  end
-
-
-  def new
-    @task = Task.new
-  end
-
-  def show
-    @task = Task.find(task_params)
-  end
-
-  def edit
-    @task = Task.find(task_params)
-  end
-
-  def create
-    Task.create(task_params)
-  end
-
-  private
-
-  def task_params
-    params.require(:task).permit(:address, :date, :description, :amount_coins)
->>>>>>> master
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
   get 'task/new'
   devise_for :users
-<<<<<<< HEAD
   root to: 'pages#home'
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
-  root to: 'tasks#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
-  resources :reviews, only: [ :new, :create, :edit, :update, :destroy ]
->>>>>>> master
 end
-
-
