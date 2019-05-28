f691e6fa1f045e1ec32e0fd369dd1245579f5f5d
William Acevedo <william.acevedo@gmail.com>
2019-05-28 18:48:11 +0200

    changes made as a result of resolving conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,14 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'profiles/edit'
-  get 'profiles/update'
-  get 'task/new'
-  devise_for :users
-  root to: 'pages#home'
+get 'profiles/edit'
+get 'profiles/update'
+get 'task/new'
 
-  resources :tasks
-  resources :offers do
-=======
   devise_for :users
   root to: 'pages#home'
   resources :tasks, only: [ :new, :index ] do
     resources :offers, only: [ :new, :create ]
   end
   resources :offers, only: [ :index, :show ] do
->>>>>>> master
     resources :reviews, only: [ :new, :create, :edit, :update, :destroy ]
   end
 end
