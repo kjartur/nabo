d600dc644fe3571a16565966119bcfab8d535f99
William Acevedo <william.acevedo@gmail.com>
2019-05-29 12:35:48 +0200

    adds logict to redirect user to correct path depending on whether they had set their bio

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,13 @@
 class ProfilesController < ApplicationController
   def show
     @user = current_user
+    if (@user.bio == nil)
+      redirect_to profiles_edit_path(@user)
+    else
+      redirect_to show_pages_path(@user)
+    end
   end
+
   def edit
   end
 
