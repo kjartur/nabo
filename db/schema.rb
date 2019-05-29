501691e9a9416edd679d9d7a8d3ba5012214495e
William Acevedo <william.acevedo@gmail.com>
2019-05-29 12:38:31 +0200

    generates additonal fields for first_name, last_name, rating, coins, address, and city to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_154517) do
+ActiveRecord::Schema.define(version: 2019_05_29_101553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,11 @@ ActiveRecord::Schema.define(version: 2019_05_28_154517) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name", default: ""
+    t.string "last_name"
+    t.integer "rating"
+    t.integer "coins"
+    t.string "address"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -59,6 +64,7 @@ ActiveRecord::Schema.define(version: 2019_05_28_154517) do
     t.datetime "updated_at", null: false
     t.string "avatar"
     t.text "bio"
+    t.string "city"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
