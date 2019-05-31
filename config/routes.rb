884785c2a3b6ebc5aed0436fb03691388a9af6f9
William Acevedo <william.acevedo@gmail.com>
2019-05-31 13:20:04 +0200

    adjusted routes to work on completed feature

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,13 @@ Rails.application.routes.draw do
   get 'pages/show'
   get 'profiles/edit'
   get 'profiles/update'
-
-
+  patch 'tasks/:id/complete', to: 'tasks#complete'
   resources :tasks do
     resources :offers, only: [ :new, :create ]
   end
   resources :offers, only: [ :index, :show ] do
     resources :reviews, only: [ :new, :create, :edit, :update, :destroy ]
   end
-
-
-  resources :complete_tasks, only: [ :update ]
 
   resource :dashboard, only: [:show]
 
