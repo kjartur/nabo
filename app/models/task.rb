f3b66bcc24ee5bcded46d39954dc235448c8f909
Julian Hermann <julian.hermann89@gmail.com>
2019-06-03 18:18:33 +0200

    complete

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,10 +3,15 @@ class Task < ApplicationRecord
   has_many :offers, :dependent => :destroy
   has_many :users, through: :offers
 
-  mount_uploader :photo, TaskPhotosUploader 
+  mount_uploader :photo, TaskPhotosUploader
 
   validates :address, :date, :description, :amount_coins, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+
+  def booked_offer
+    offers.where(state: 'booked').first
+  end
 end
