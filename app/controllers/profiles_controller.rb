5b407d852c11c84ad00266d9a65ea5e69c041655
Efim Shliamin <shliamin@icloud.com>
2019-06-03 18:12:48 +0200

    avatars are ckickable

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,6 @@
 class ProfilesController < ApplicationController
   def show
-    @user = current_user
-    if (@user.bio == nil)
-      redirect_to profiles_edit_path(@user)
-    else
-      redirect_to show_pages_path(@user)
-    end
+    @user = User.find(params[:id])
   end
 
   def edit
