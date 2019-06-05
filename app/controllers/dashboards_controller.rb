624a6ee308f15655824b4698f70d8ef3f4398280
Efim Shliamin <shliamin@icloud.com>
2019-06-05 12:41:59 +0200

    new dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,13 +3,23 @@ class DashboardsController < ApplicationController
   def show
     @tasks = current_user.tasks
     @offers = current_user.offers
+    # @tasks_array = []
 
-    @offers.each do |offer|
-    @task = Task.find(id = offer.task_id)
-    end
+    # @offers.each do |offer|
+    #   task = Task.find(offer.task_id)
+    #   @tasks_array << task
+    # end
+  end
+
+    # def find_profile_pic(offer)
+    #   task = Task.find(offer.task_id)
+    #   user_id = task.user_id
+    #   User.find(user_id).avatar
+    #   raise
+    # end
+
     # @offers.each do |offer|
     #   @user = User.find(task_id=offer.task_id)
     # end
-  end
 
 end
