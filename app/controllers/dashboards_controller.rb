7e0e36721d38e341667eee91135acdeaf8d3af3c
Efim Shliamin <shliamin@icloud.com>
2019-06-05 10:40:43 +0200

    this is avatar

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,6 +3,13 @@ class DashboardsController < ApplicationController
   def show
     @tasks = current_user.tasks
     @offers = current_user.offers
+
+    @offers.each do |offer|
+    @task = Task.find(id = offer.task_id)
+    end
+    # @offers.each do |offer|
+    #   @user = User.find(task_id=offer.task_id)
+    # end
   end
 
 end
